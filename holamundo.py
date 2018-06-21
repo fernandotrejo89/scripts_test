c0191914474c3028a99a058cfc18a7542566c42b
Fernando Trejo <fernandotrejoyari@gmail.com>
2018-06-21 17:27:18 -0500

    cargando holamundo.py

---
+++ holamundo.py
@@ -0,0 +1,12 @@
+# variables, listas, dictionaries, loops (for)
+
+print('Hola Mundo!')
+
+empleados = ['Fernando','Victor','Rocio','Luis']
+
+print(empleados)
+
+for i in empleados:
+    print(i)
+
+print('imprimimos todos los empleados')
